2cfcc65fce8bc9ec4a53993ed0d3c38ae75dd918
Samuel Coco <Samuel.HenriAC@outlook.com>
2022-12-17 20:14:21 +0000

    Project initialization

---
+++ src/main/java/com/samhcoco/project/searchframe/SearchframeApplication.java
@@ -0,0 +1,13 @@
+package com.samhcoco.project.searchframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchframeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samhcoco/project/searchframe/SearchframeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhcoco.project.searchframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchframeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
